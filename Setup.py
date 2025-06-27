d6d4f9ae9abeca00f8609f9cd198dbabcd1102d6
Maahircabdi368 <mosharro599@gmail.com>
2025-06-27 09:41:30 +0300

    Requirements.txt

--- Setup.py
+++ Setup.py
@@ -1,19 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="scanner-website",
-    version="0.1",
-    author="Maahircabdi368",
-    description="Simple IP port scanner tool",
-    py_modules=["main"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires='>=3.6',
-    entry_points={
-        'console_scripts': [
-            'scanner=main:scan_ip',
-        ],
-    },
-)
